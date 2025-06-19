f781e930b726e2c78591a4fb50d781e681421020
Bill <bill@thewallisfamily.co.uk>
2025-06-19 09:06:29 +0100

    test: improve test parameterisation

--- tests/pre_commit_validator/test__hook.py
+++ tests/pre_commit_validator/test__hook.py
@@ -12,51 +12,49 @@ from bills_hooks.pre_commit_validate import hook
 FIXTURES = pathlib.Path(__file__).parent / "fixtures"
 
 
-@pytest.fixture
-def valid_config_files(tmp_path: pathlib.Path):
+@pytest.fixture(
+    params=[
+        "valid.1.pre-commit-config.yaml",
+        "valid.2.pre-commit-config.yaml",
+    ]
+)
+def valid_config_file(
+    tmp_path: pathlib.Path,
+    request: pytest.FixtureRequest,
+) -> pathlib.Path:
     """
-    Valid pre-commit configuration files.
+    Valid pre-commit configuration file.
     """
 
-    config_file_1 = tmp_path / "valid.1.pre-commit-config.yaml"
-    config_file_2 = tmp_path / "valid.2.pre-commit-config.yaml"
-    shutil.copyfile(
-        FIXTURES / "valid.1.pre-commit-config.yaml",
-        config_file_1,
-    )
-    shutil.copyfile(
-        FIXTURES / "valid.2.pre-commit-config.yaml",
-        config_file_2,
-    )
+    filename = request.param
+    config_file = tmp_path / filename
+    shutil.copyfile(FIXTURES / filename, config_file)
 
-    return [config_file_1, config_file_2]
+    return config_file
 
 
 @pytest.fixture
-def invalid_config_file(tmp_path: pathlib.Path):
+def invalid_config_file(tmp_path: pathlib.Path) -> pathlib.Path:
     """
     An invalid pre-commit configuration file.
     """
 
-    config_file = tmp_path / "invalid.pre-commit-config.yaml"
-    shutil.copyfile(
-        FIXTURES / "invalid.pre-commit-config.yaml",
-        config_file,
-    )
+    filename = "invalid.pre-commit-config.yaml"
+    config_file = tmp_path / filename
+    shutil.copyfile(FIXTURES / filename, config_file)
 
     return config_file
 
 
 def test__valid_config_passes(
-    valid_config_files: list[pathlib.Path],
+    valid_config_file: pathlib.Path,
 ):
     """
     A valid pre-commit configuration file passes validation.
     """
 
-    for valid_config_file in valid_config_files:
-        rc = hook.main(["--pre-commit-config-path", str(valid_config_file)])
-        assert rc == hook.SUCCESS
+    rc = hook.main(["--pre-commit-config-path", str(valid_config_file)])
+    assert rc == hook.SUCCESS
 
 
 def test__invalid_config_fails(
