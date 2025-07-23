52b113f2e1da7dbea10fc3be1113eaf6fd549ec8
Bill Wallis <bill@tasman.ai>
2025-07-23 21:17:15 +0100

    feat: replace custom pre-commit CI config validator

--- bills_hooks/__init__.py
+++ bills_hooks/__init__.py
@@ -4,7 +4,5 @@ Pre-commit hooks for my own projects.
 
 import pathlib
 
-PROJECT_ROOT = pathlib.Path(
-    __file__
-).parent.parent  # resolves to `bills-hooks/`
+PROJECT_ROOT = pathlib.Path(__file__).parent.parent
 SOURCE_ROOT = PROJECT_ROOT / "bills_hooks"
